c8d0857624dd9a53e2a154bb1ad3cfa8dbabbb17
Dwiki Nurkurniawan <nurkurniawandwiki@gmail.com>
2018-05-09 12:33:18 +0700

    move desktop_bg to Client folder, fix error on main.py

--- main.py
+++ main.py
@@ -16,18 +16,18 @@ def api_login():
 		cursor = mysql.connect().cursor()
 		uname = request.form['username']
 		password = request.form['password']
-		password = hashlib.md5(password.encode()).hexdigest()
+		# password = hashlib.md5(password.encode()).hexdigest()
 		cursor.execute("SELECT * FROM `user` WHERE `uname`='%s' AND `pass`='%s'" % (uname, password))
 		data = cursor.fetchone()
 		if data:
-			return json.dumps(data)
+			return json.dumps({"uid":data[0], "email":data[3]})
 		else:
 			return json.dumps({"status":0})
 	elif request.method == 'GET':
-		return "NOT ALLOWED"
+		abort(405)
 
 @app.route('/api/register', methods=['POST'])
-def api_register(uname, password, email):
+def api_register():
 	password = hashlib.md5(password.encode()).hexdigest()
 	conn = mysql.connect()
 	cursor = conn.cursor()
@@ -36,37 +36,25 @@ def api_register(uname, password, email):
 		conn.commit()
 		return json.dumps({"status":1})
 	except Exception as e:
-		return json.dumps({"status":0})
+		if "Duplicate" in str(e):
+			return json.dumps({"status":2})
+		else:
+			return json.dumps({"status":0})
 
-@app.route('/api/insert', methods=['GET'])
-def api_insert():
+@app.route('/api/insert/<uid>', methods=['GET'])
+def api_insert(uid):
 	conn = mysql.connect()
 	cursor = conn.cursor()
 	try:
-		folder = request.args.get('folder')
-		title = request.args.get('title')
-		last_position = request.args.get('last_position')
-		duration = request.args.get('duration')
-		file_id = folder + len(folder) + title
-		print(file_id)
-		# cursor.execute("INSERT INTO `list` VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % ())
-	except Exception as e:
-		raise e
-
-@app.route('/api/update/<uid>', methods=['GET'])
-def api_update(uid):
-	conn = mysql.connect()
-	cursor = conn.cursor()
-	try:
-		folder = request.args.get('folder')
-		title = request.args.get('title')
-		last_position = request.args.get('last_position')
-		duration = request.args.get('duration')
-		cursor.execute("SELECT * FROM list WHERE id_user='%s' AND title='%s'" % (uid, title))
-		data = cursor.fetchone()
-		if data:
-			cursor.execute("REPLACE INTO list ()")
-
+		folder = request.args['folder']
+		title = request.args['title']
+		last_position = request.args['last_position']
+		duration = request.args['duration']
+		temp = title
+		file_id = hashlib.md5(temp.encode()).hexdigest()
+		cursor.execute("INSERT INTO `list` VALUES ('%s', '%s', '%s', '%s', '%s', '%s') ON DUPLICATE KEY UPDATE folder='%s', title='%s', last_position='%s', duration='%s'" 
+			% (file_id, uid, folder, title, last_position, duration, folder, title, last_position, duration))
+		conn.commit()
 		return json.dumps({"status":1})
 	except Exception as e:
 		return json.dumps({"status":0})
